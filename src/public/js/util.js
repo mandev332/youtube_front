6bb0d9a32304db141d5246713a6efe11165395d3
mandev332 <https://github.com/mandev332>
2022-03-15 16:04:42 +0300

    api update

--- src/public/js/util.js
+++ src/public/js/util.js
@@ -1,27 +1,27 @@
-const backendApi = "http://192.168.3.4:4000";
+const backendApi = "https://youtube-app-mandev.herokuapp.com";
 
 async function request(route, method, body) {
-  let headers = {
-    token: window.localStorage.getItem("token"),
-  };
+    let headers = {
+        token: window.localStorage.getItem("token"),
+    };
 
-  if (!(body instanceof FormData)) {
-    headers["Content-Type"] = "application/json";
-  }
+    if (!(body instanceof FormData)) {
+        headers["Content-Type"] = "application/json";
+    }
 
-  let response = await fetch(backendApi + route, {
-    method,
-    headers,
-    body: body instanceof FormData ? body : JSON.stringify(body),
-  });
+    let response = await fetch(backendApi + route, {
+        method,
+        headers,
+        body: body instanceof FormData ? body : JSON.stringify(body),
+    });
 
-  if (!(response.status === 200 || response.status === 201)) {
-    response = await response.json();
-    throw new Error(response.message);
-  }
-  return await response.json();
+    if (!(response.status === 200 || response.status === 201)) {
+        response = await response.json();
+        throw new Error(response.message);
+    }
+    return await response.json();
 }
 
 function createElements(...array) {
-  return array.map((el) => document.createElement(el));
+    return array.map((el) => document.createElement(el));
 }
